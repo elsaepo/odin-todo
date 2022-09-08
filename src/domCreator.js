544303e8a8d9e0f36e61d0c16e5564a62f15bc38
Carl Madsen <madsencarlb@gmail.com>
2022-09-08 23:47:05 +1000

    add barebones sidebar navigation

--- src/domCreator.js
+++ src/domCreator.js
@@ -14,6 +14,30 @@ body.id= "body-container";
 
 const sidebar = document.createElement("div");
 sidebar.id = "sidebar";
+
+const navContainer = document.createElement("div");
+navContainer.classList.add("nav-container");
+const navArray = [
+    { title: "All", icon: "a", link: "all" },
+    { title: "Today", icon: "b", link: "today" },
+    { title: "Week", icon: "c", link: "week" },
+    { title: "Important", icon: "d", link: "important" },
+    { title: "Completed", icon: "e", link: "completed" }
+];
+navArray.forEach(obj => {
+    const navButton = document.createElement("div");
+    navButton.id = obj.link;
+    navButton.classList.add("nav-button")
+    const navIcon = document.createElement("p");
+    navIcon.textContent = obj.icon;
+    const navText = document.createElement("h3");
+    navText.textContent = obj.title;
+    navButton.appendChild(navIcon);
+    navButton.appendChild(navText);
+    navContainer.appendChild(navButton);
+})
+
+sidebar.appendChild(navContainer);
 body.appendChild(sidebar);
 
 
